a090e5068b5bf33681bee9a667e86165083ef12b
edislainegodoy <edislainegodoy@hotmail.com>
2018-06-16 22:45:56 -0300

    att 16/06

--- app/src/main/java/com/example/isaofelipemorigaki/ewe/ColaboradorActivity.java
+++ app/src/main/java/com/example/isaofelipemorigaki/ewe/ColaboradorActivity.java
@@ -1,38 +1,30 @@
 package com.example.isaofelipemorigaki.ewe;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.view.View;
-import android.widget.Button;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageButton;
 
-public class ColaboradorActivity extends AppCompatActivity {
+public class ColaboradorActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colaborador);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        ImageButton botao_historico = findViewById(R.id.botao_historico);
-        botao_historico.setOnClickListener(new ImageButton.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(ColaboradorActivity.this, HistoricoActivity.class);
-                startActivity(i);
-            }
-        });
-
-        ImageButton botao_validar = findViewById(R.id.botao_validar);
-        botao_validar.setOnClickListener(new ImageButton.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(ColaboradorActivity.this, ValidarActivity.class);
-                startActivity(i);
-            }
-        });
-
-        ImageButton botao_colaborar = findViewById(R.id.botao_colaborar);
+        FloatingActionButton botao_colaborar = findViewById(R.id.fab);
         botao_colaborar.setOnClickListener(new ImageButton.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -40,5 +32,63 @@ public class ColaboradorActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.colaborado, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_historico) {
+            // Handle the camera action
+        } else if (id == R.id.nav_sair) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
--- app/src/main/java/com/example/isaofelipemorigaki/ewe/ContribuirActivity.java
+++ app/src/main/java/com/example/isaofelipemorigaki/ewe/ContribuirActivity.java
@@ -1,13 +1,70 @@
 package com.example.isaofelipemorigaki.ewe;
 
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class ContribuirActivity extends AppCompatActivity {
+import com.example.isaofelipemorigaki.ewe.GD.AppDataBase;
+import com.example.isaofelipemorigaki.ewe.GD.Beacons;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ContribuirActivity extends AppCompatActivity implements BeaconConsumer, RangeNotifier {
+    private BeaconManager mBeaconManager;
+    private String ultimoBeacon;
+    private AutoCompleteTextView campoBeacon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contribuir);
+
+        Button botao_contribuir = findViewById(R.id.btnContribuir);
+
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        // Encapsulates a beacon identifier of arbitrary byte length
+        ArrayList<Identifier> identifiers = new ArrayList<>();
+
+        // Set null to indicate that we want to match beacons with any value
+        identifiers.add(null);
+        // Represents a criteria of fields used to match beacon
+        Region region = new Region("AllBeaconsRegion", identifiers);
+        try {
+            // Tells the BeaconService to start looking for beacons that match the passed Region object
+            mBeaconManager.startRangingBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        // Specifies a class that should be called each time the BeaconService gets ranging data, once per second by default
+        mBeaconManager.addRangeNotifier(this);
+    }
+
+    @Override
+    public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+        if (beacons.size() > 0) {
+            Beacon beacon = beacons.iterator().next();
+            Identifier instance = beacon.getId2();
+            double distancia = beacon.getDistance();
+            campoBeacon = findViewById(R.id.identificacaoDispositivo);
+            if (!instance.toString().equals(ultimoBeacon) && distancia <= 6){
+                Beacons beaconDetectado = AppDataBase.getDatabase(this).beaconDAO().findByInstance(instance.toString());
+                campoBeacon.setText(beaconDetectado.getId());
+            }
+        }
     }
 }
